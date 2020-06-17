1924797ab57c84391b39316e532f295308084222
PasinduDew <pasindudewapriya96@gmail.com>
2020-06-17 20:53:00 +0530

    Set Up Project Structure

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import Axios from "axios";
 
 Vue.config.productionTip = false;
+
+// Global Axios Configurations
+Vue.prototype.$http = Axios.create({
+  baseURL: "http://localhost:3000"
+});
+
+// Event Bus
+export const bus = new Vue();
 
 new Vue({
   router,
