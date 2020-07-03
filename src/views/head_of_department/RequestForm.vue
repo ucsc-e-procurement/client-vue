d4d9823e16fc56358c05c4730c096666b6d4d584
Prashastha Mudannayake <prashasthamudannayake98@gmail.com>
2020-07-03 08:40:20 +0530

    form validation

--- src/views/head_of_department/RequestForm.vue
+++ src/views/head_of_department/RequestForm.vue
@@ -1,6 +1,6 @@
 <template>
   <ValidationObserver ref="observer" v-slot="{}">
-    <form>
+    <form id="request-form">
       <v-card class="overflow-hidden" color="grey lighten-1" dark>
         <v-toolbar flat outlined color="primary" font="balck">
           <v-icon>mdi-cards-variant</v-icon>
@@ -8,16 +8,11 @@
             >Request Form</v-toolbar-title
           >
           <v-spacer></v-spacer>
-          <v-btn color="grey " fab small @click="isEditing = !isEditing">
-            <v-icon v-if="isEditing">mdi-close</v-icon>
-            <v-icon v-else>mdi-pencil</v-icon>
-          </v-btn>
         </v-toolbar>
         <v-card-text>
           <ValidationProvider v-slot="{ errors }" name="Name" rules="required">
             <v-text-field
               v-model="name"
-              :disabled="!isEditing"
               color="black"
               label="Procurement Name"
               required
@@ -26,7 +21,6 @@
           </ValidationProvider>
           <ValidationProvider v-slot="{ errors }" name="Type" rules="required">
             <v-autocomplete
-              :disabled="!isEditing"
               v-model="selectProcType"
               :items="statesProcType"
               :filter="customFilter"
@@ -37,22 +31,15 @@
               :error-messages="errors"
             ></v-autocomplete>
           </ValidationProvider>
-          <ValidationProvider
-            v-slot="{ errors }"
-            name="Method"
-            rules="required"
-          >
-            <v-autocomplete
-              :disabled="!isEditing"
-              v-model="selectProcMethod"
-              :items="statesProcMethod"
-              :filter="customFilter"
-              color="black"
-              item-text="name"
-              label="Procurment Method"
-              required
+          <ValidationProvider v-slot="{ errors }" name="checkbox">
+            <v-checkbox
+              v-model="checkbox"
               :error-messages="errors"
-            ></v-autocomplete>
+              value="false"
+              label="Re-order"
+              type="checkbox"
+              required
+            ></v-checkbox>
           </ValidationProvider>
           <ValidationProvider
             v-slot="{ errors }"
@@ -60,7 +47,6 @@
             rules="required"
           >
             <v-textarea
-              :disabled="!isEditing"
               v-model="descript"
               outlined
               name="input-7-4"
@@ -73,20 +59,13 @@
         <v-divider></v-divider>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn :disabled="!isEditing" color="success" @click="save"
-            >Save</v-btn
-          >
+          <v-btn color="success" @click.stop="dialog = true">Submit</v-btn>
         </v-card-actions>
-        <v-snackbar v-model="hasSaved" :timeout="2000" absolute bottom left
-          >Your request has been updated</v-snackbar
-        >
       </v-card>
       <v-row justify="center">
-        <v-btn color="warning" dark @click.stop="dialog = true">Submit</v-btn>
-
         <v-dialog v-model="dialog" max-width="290">
           <v-card>
-            <v-card-title class="headline">Submit Request?</v-card-title>
+            <v-card-title class="headline">Confirm Request?</v-card-title>
 
             <v-card-text
               >By proceeding you are accepting to initialize a new procurement
@@ -96,7 +75,12 @@
             <v-card-actions>
               <v-spacer></v-spacer>
 
-              <v-btn color="green darken-1" text @click="submit" to="/hod"
+              <v-btn
+                class="mr-4"
+                color="green darken-1"
+                text
+                @click="submit"
+                to="/hod"
                 >Proceed</v-btn
               >
 
@@ -119,6 +103,7 @@ import {
   ValidationProvider,
   setInteractionMode,
 } from "vee-validate";
+import axios from "axios";
 
 setInteractionMode("eager");
 
@@ -134,32 +119,33 @@ export default {
     return {
       name: "",
       selectProcType: null,
-      selectProcMethod: null,
       descript: "",
       dialog: false,
-      hasSaved: false,
-      isEditing: null,
       model: null,
       statesProcType: [
         { name: "Services", abbr: "S", id: 1 },
         { name: "Work", abbr: "W", id: 2 },
         { name: "Goods", abbr: "G", id: 3 },
       ],
-      statesProcMethod: [
-        { name: "Direct Method", abbr: "DM", id: 1 },
-        { name: "National Shopping", abbr: "NS", id: 2 },
-        { name: "National Competitive Bidding", abbr: "NCB", id: 3 },
-      ],
+      checkbox: false,
+      division: "ENG",
+      head_of_division_id: "emp00004",
     };
   },
 
   methods: {
     submit() {
       this.$refs.observer.validate();
-    },
-    save() {
-      this.isEditing = !this.isEditing;
-      this.hasSaved = true;
+      axios
+        .post("http://localhost:5001/api/hod/create_req", {
+          //body
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
 };
