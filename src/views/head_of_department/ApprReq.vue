946a25332d756fd850cc3684baf575a5b3ff6abb
Prashastha Mudannayake <prashasthamudannayake98@gmail.com>
2020-07-05 16:34:36 +0530

    Port correction

--- src/views/head_of_department/ApprReq.vue
+++ src/views/head_of_department/ApprReq.vue
@@ -9,11 +9,7 @@
       single-line
       hide-details
     ></v-text-field>
-    <v-data-table
-      :headers="headers"
-      :items="procs"
-      :search="search"
-    ></v-data-table>
+    <v-data-table :headers="headers" :items="procs" :search="search"></v-data-table>
   </v-card>
 </template>
 
@@ -30,21 +26,21 @@ export default {
           text: "Request Id",
           align: "start",
           sortable: false,
-          value: "requisition_id",
+          value: "requisition_id"
         },
         { text: "Description", value: "description" },
-        { text: "Date", value: "date" },
+        { text: "Date", value: "date" }
       ],
-      procs: [],
+      procs: []
     };
   },
   created() {
     axios
-      .get(`http://localhost:5001/api/hod/approved/${this.empid}`)
-      .then((response) => {
+      .get(`http://localhost:5000/api/hod/approved/${this.empid}`)
+      .then(response => {
         this.procs = response.data;
       })
-      .catch((error) => console.log(error));
-  },
+      .catch(error => console.log(error));
+  }
 };
 </script>
