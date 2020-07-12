b9c8672911cabb5c1ab921c74102a7d405bd68ca
sachininayanathara <sach.nayanathara1995@gmail.com>
2020-07-12 21:10:26 +0530

    purchase order- UI

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ import Admin_Procurement_Overview from "../views/admin/Procurement_Overview.vue"
 import Admin_View_Suppliers from "../views/admin/View_Suppliers.vue";
 import Admin_View_Users from "../views/admin/Users.vue";
 import Admin_Products from "../views/admin/Products.vue";
+import Admin_Purchase_Order from "../views/admin/Purchase_Order.vue";
 
 // Head of department
 import HOD from "../views/head_of_department/Head_Of_Department.vue";
@@ -141,6 +142,12 @@ const routes = [
         path: "products",
         name: "view all products",
         component: Admin_Products,
+        props: true
+      },
+      {
+        path: "purchase_order",
+        name: "generate purchase order",
+        component: Admin_Purchase_Order,
         props: true
       },
       // TODO Redirect to Page Not Found Or Dashborad
