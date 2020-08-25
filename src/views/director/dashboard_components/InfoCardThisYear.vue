902cfa3cf53cefe5f2b5163142a06dbdd97eb2a8
Tharinda Dilshan Piyadasa <tharindad7@gmail.com>
2020-08-25 23:27:32 +0530

    Director | Current Year stats

--- src/views/director/dashboard_components/InfoCardThisYear.vue
+++ src/views/director/dashboard_components/InfoCardThisYear.vue
@@ -10,7 +10,7 @@
                     <div>
                       <v-card-title class="headline font-weight-bold" style="padding-top: 40px;">
                         <div style="font-size: 50px;">
-                          60
+                          {{this.supplierCount}}
                         </div>
                       </v-card-title>
                       <v-card-actions>
@@ -42,7 +42,7 @@
                     <div>
                       <v-card-title class="headline font-weight-bold" style="padding-top: 40px;">
                         <div style="font-size: 50px;">
-                          20
+                          {{this.requisitionCount}}
                         </div>
                       </v-card-title>
                       <v-card-actions>
@@ -74,7 +74,7 @@
                     <div>
                       <v-card-title class="headline font-weight-bold" style="padding-top: 40px;">
                         <div style="font-size: 50px;">
-                          70%
+                          {{Math.round(this.completedProcurementCount * 100 *100)/100}}%
                         </div>
                       </v-card-title>
                       <v-card-actions>
@@ -106,7 +106,7 @@
                     <div>
                       <v-card-title class="headline font-weight-bold" style="padding-top: 40px;">
                         <div style="font-size: 50px;">
-                          30%
+                          {{Math.round(this.terminatedProcurementCount * 100)/100}}%
                         </div>
                       </v-card-title>
                       <v-card-actions>
@@ -150,15 +150,53 @@ export default {
 
   // Data Variables and Values
   data: () => ({
-    //
+    requisitionCount: '',
+    ongoingProcurementCount: '',
+    completedProcurementCount: '',
+    terminatedProcurementCount: '',
+    totalProcurements: '',
+    supplierCount: '',
+    isMounted: false,
   }),
 
   // Custom Methods and Functions
-  methods: {},
+  methods: {
+    getProcurements(){
+      this.$http
+        .get("/api/director/procurements")
+        .then(response => {
+          console.log(response);
+          var totalProcurements = response.data.filter(item => item.status == 'on-going').length + response.data.filter(item => item.status == 'completed').length + response.data.filter(item => item.status == 'terminated').length
+          this.ongoingProcurementCount = response.data.filter(item => item.status == 'on-going').length / totalProcurements;
+          this.completedProcurementCount = response.data.filter(item => item.status == 'completed').length / totalProcurements;
+          this.terminatedProcurementCount = response.data.filter(item => item.status == 'terminated').length / totalProcurements;
+          this.requisitionCount = response.data.filter(item => new Date(item.date).getFullYear() == new Date().getFullYear()).length;
+          // this.isMounted = true;
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    },
+    getSuppliers() {
+      this.$http
+        .get("/api/director/get_suppliers")
+        .then(response => {
+          // console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>", response);
+          this.supplierCount = response.data.length
+          // this.isMounted = true;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  },
 
   // Life Cycle Hooks
   beforeCreate() {},
-  created() {},
+  created() {
+    this.getProcurements();
+    this.getSuppliers();
+  },
   beforeMount() {},
   mounted() {},
   beforeUpdate() {},
